f32641295bb0be3636ce0c447de9d169f01224b7
Brandon2002 <btapia@ucol.mx>
2020-11-26 15:23:09 -0600

    implemented convertir

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 export default class App {
-
+    convertir(pesos){
+        let dolar = 20.5;
+        let euro = 22.7;
+        let resultado;
+        if(pesos < 2000){
+            resultado = pesos / dolar;
+        } else if (pesos > 2000){
+            resultado = pesos / euro;
+        } return resultado;
+    }
 }
 
 let app = new App();
+console.log(app.convertir(1900));
+console.log(app.convertir(2400));

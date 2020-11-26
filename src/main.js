8404328dde3a3cb40fa2303bee5cc40c82e2cbfc
Brandon2002 <btapia@ucol.mx>
2020-11-26 15:48:09 -0600

    implemented convertir()

--- src/main.js
+++ src/main.js
@@ -4,13 +4,32 @@ export default class App {
         let euro = 22.7;
         let resultado;
         if(pesos < 2000){
-            resultado = pesos / dolar;
-        } else if (pesos > 2000){
-            resultado = pesos / euro;
+            resultado = pesos * dolar;
+        } else if (pesos >= 2000){
+            resultado = pesos * euro;
         } return resultado;
     }
+    costoRenta(kilometros){
+        let resultado;
+        if (kilometros < 50){
+            resultado = kilometros * 3;
+        }else if(kilometros >= 50 && kilometros < 100){
+            resultado = kilometros * 5;
+        }else if (kilometros >= 100 && kilometros < 200){
+            resultado = kilometros * 6;
+        }else if (kilometros >= 200){
+            resultado = kilometros * 6.5;
+        }return resultado;
+    }
+
 }
 
 let app = new App();
-console.log(app.convertir(1900));
+console.log(app.convertir(2000));
 console.log(app.convertir(2400));
+
+console.log(app.costoRenta(49));
+console.log(app.costoRenta(55));
+console.log(app.costoRenta(100));
+console.log(app.costoRenta(150));
+console.log(app.costoRenta(210));
